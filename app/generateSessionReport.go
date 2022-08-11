f2f64e78a42c37d5b80fb56d90849774150dc259
Havoczephyr <havoczephyr@hotmail.com>
2022-08-11 19:27:17 -0400

    html export init commit on branch.

--- app/generateSessionReport.go
+++ app/generateSessionReport.go
@@ -43,21 +43,48 @@ func generateSessionReport(gapArr []keypressGap, sessName, sessPath string, bool
 	averageGapDurationDelta = durationTotal / float32(len(gapArr))
 
 	slipKeyHeader := make([]string, 0)
+	// slipKeyHeader = append(slipKeyHeader,
+	// 	"# SlipKey Report\n",
+	// 	fmt.Sprintf("## Session Name - %s\n", sessName),
+	// 	fmt.Sprintf("### Entry Date - %d %d %d\n", now.Month(), now.Day(), now.Year()),
+	// 	"\n",
+	// 	fmt.Sprintf("postprocessed-fixed.tsv generated?: %s\n", tsvGenerated),
+	// 	"\n",
+	// 	fmt.Sprintf("- Amount of Missing Keypresses: %d\n", missingKeyTotal),
+	// 	fmt.Sprintf("- Average Keypress Gap Size: %.2f\n", keypressGapAvg),
+	// 	fmt.Sprintf("- Average Interrupt Time Δ: %.3f\n", averageInterruptDelta),
+	// 	fmt.Sprintf("- Smallest Interrupt Threshold: %.3f\n", smallestInterruptThreshold),
+	// 	fmt.Sprintf("- Average Gap Duration: %.3f\n", averageGapDurationDelta),
+	// )
 	slipKeyHeader = append(slipKeyHeader,
-		"# SlipKey Report\n",
-		fmt.Sprintf("## Session Name - %s\n", sessName),
-		fmt.Sprintf("### Entry Date - %d %d %d\n", now.Month(), now.Day(), now.Year()),
-		"\n",
-		fmt.Sprintf("postprocessed-fixed.tsv generated?: %s\n", tsvGenerated),
-		"\n",
-		fmt.Sprintf("- Amount of Missing Keypresses: %d\n", missingKeyTotal),
-		fmt.Sprintf("- Average Keypress Gap Size: %.2f\n", keypressGapAvg),
-		fmt.Sprintf("- Average Interrupt Time Δ: %.3f\n", averageInterruptDelta),
-		fmt.Sprintf("- Smallest Interrupt Threshold: %.3f\n", smallestInterruptThreshold),
-		fmt.Sprintf("- Average Gap Duration: %.3f\n", averageGapDurationDelta),
+		"<!DOCTYPE html>\n",
+		fmt.Sprintf(`<html lang="en">`+"\n"),
+		"<head>\n",
+		fmt.Sprintf(`<meta charset="UTF-8">`+"\n"),
+		fmt.Sprintf(`<meta http-equiv="X-UA-Compatible" content="IE=edge">`+"\n"),
+		fmt.Sprintf(`<meta name="viewport" content="width=device-width, initial-scale=1.0">`+"\n"),
+		"<title>slipKey Report</title>\n",
+		fmt.Sprintf(`<link rel="stylesheet" href="style.css">`+"\n"),
+		"</head>\n",
+		"<body>\n",
+		fmt.Sprintf(`<img src="TG-horizontal_logo.svg">`+"\n"),
+		"<h1>slipKey Report</h1>\n",
+		"<hr>\n",
+		fmt.Sprintf("<h2>Session Name - %s</h2>\n", sessName),
+		fmt.Sprintf("<h3>Date %d %d %d</h3>\n", now.Month(), now.Day(), now.Year()),
+		fmt.Sprintf("<p><b>postprocessed-fixed.tsv generated?</b>: %s</p>\n", tsvGenerated),
+		"<ul>\n",
+		fmt.Sprintf("<li>Amount of Missing Keypresses: %d</li>\n", missingKeyTotal),
+		fmt.Sprintf("<li>Average Keypress Gap size: %.2f</li>\n", keypressGapAvg),
+		fmt.Sprintf("<li>Average Interrupt Time Δ: %.3f</li>\n", averageInterruptDelta),
+		fmt.Sprintf("<li>Smallest Interrupt Threshold: %.3f</li>\n", smallestInterruptThreshold),
+		fmt.Sprintf("<li>Average Gap Duration: %.3f</li>\n", averageGapDurationDelta),
+		"</ul>\n",
+		"<hr>\n",
+		"</body>\n",
+		"</html>\n",
 	)
-
-	report, err := os.Create(fmt.Sprintf("%s/SlipKey-Report-%s.md", folderStr, sessName))
+	report, err := os.Create(fmt.Sprintf("%s/SlipKey-Report-%s.html", folderStr, sessName))
 	if err != nil {
 		fmt.Printf("failed to create Report -%v", err)
 	}
@@ -66,10 +93,13 @@ func generateSessionReport(gapArr []keypressGap, sessName, sessPath string, bool
 	for _, str := range slipKeyHeader {
 		report.WriteString(str)
 	}
+
 	for index, gap := range gapArr {
-		report.WriteString(fmt.Sprintf("\n---\n**Gap Set #%d**\n", index+1))
+		report.WriteString(fmt.Sprintf("\n<hr>\n<b>Gap Set %d</b>\n", index+1))
+		report.WriteString("<ul>\n")
 		for _, item := range gap.MissingKeypresses {
-			report.WriteString(fmt.Sprintf("- %.3f\t%.3f\t%s\n", item.TBegin, item.TEnd, item.Content))
+			report.WriteString(fmt.Sprintf("<li>%.3f\t%.3f\t%s</li>\n", item.TBegin, item.TEnd, item.Content))
 		}
+		report.WriteString("</ul>\n")
 	}
 }
